8ec93d3b7ae7deb85474e1d33278a7daebfa0fa2
William ANGER <anger.william@gmail.com>
2014-09-18 19:53:11 +0200

    (cleanup) again some comments and testing
    seems like I'm missing afew commands because it doesn't work as expected

--- bb2gh.sh
+++ bb2gh.sh
@@ -3,9 +3,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir ~/bb2gh_temp
 cd ~/bb2gh_temp
+
+# clone all bitbucket repos
 mkdir .bitbucket
 cd .bitbucket/
-git clone git clone git@bitbucket.org:smartificiel/blep-android.git/wiki
+
+# wikis
 git clone git@bitbucket.org:smartificiel/blep-android.git/wiki
 ls -al
 mv wiki android.wiki
@@ -19,26 +22,27 @@ mv wiki general.wiki
 ls -al
 ls -al general.wiki/
 cd ~/bb2gh_temp
+
+# sources
+git clone git@bitbucket.org:smartificiel/blep-android.git
+git clone git@bitbucket.org:smartificiel/blep-arduino.git
+git clone git@bitbucket.org:smartificiel/blep-general.git
+
+# downloads
+# ???
+
+# issues?
+# ???
+
+# prepare destination repos
 mkdir .github
-cd .github/
-ls -al
-cd test/
-ls -al
-touch README.md
-ls -al
-git init
-git add README.md 
-git commit -am "Initial commit"
-git status
-git remote add -f android ~/bb2gh_temp/.android/blep-android
-git merge android/master 
+cd .github
 
-ls -al
-
-cd ..
-rm -rf test
+# test folder for repo
 mkdir test
-cd test/
+cd test
+
+# initialize repo with dummy readme file
 git init
 touch README.md
 git add README.md 
@@ -47,21 +51,16 @@ git commit -m "Initial commit"
 mkdir android
 mkdir arduino
 mkdir general
+
 ls -al
 cd android/
-git remote add -f android ~/bb2gh_temp/.android/blep-android
+git remote add -f android ~/bb2gh_temp/blep-android
 ls -al
 cd ..
 ls -al
 git merge android/master 
 ls -al
-mv Speech_Recognition android/
-mv VoiceRecognitio android/
-mv VoiceRecognition android/
-ls -al
 
-git reset --hard
-ls -al
 git rm README.md 
 git commit -m "deleted dummy readme file"
 dir -exclude android | %{git mv $_.Name android}
@@ -78,7 +77,6 @@ rm -rf test/
 git clone git@github.com:ideasmashup/test.git
 cd test/
 ls -al
-git remote add -f projectb /path/to/other/repo
 git remote add -f android ~/bb2gh_temp/.android/blep-android
 ls -al
 git merge -s ours --no-commit android/master
@@ -87,7 +85,6 @@ ls -al
 git commit -m "Merge bitbucket blep-android into android/"
 
 ls -al
-git remote add -f  /path/to/other/repo
 git merge -s ours --no-commit arduino/master
 git read-tree --prefix=arduino/ -u arduino/master
 
